442e3a282a917f7aebe78166c8f871559013517c
LayWasTaken <clarencetoledo08@gmail.com>
2022-08-26 12:40:16 +0800

    Sending events to clients

---
+++ src/Events.php
@@ -0,0 +1,63 @@
+<?php
+namespace HTNProtocol;
+
+use HTNProtocol\Models\PlayerEvent;
+use HTNProtocol\Models\PlayerMessageEvent;
+use PDO;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerChatEvent;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\event\player\PlayerQuitEvent;
+use poggit\libasynql\DataConnector;
+
+class Events implements Listener
+{
+    public function __construct(
+        private ClientSocket $sock,
+        private DataConnector $db
+    ) {
+    }
+
+    public function onJoin(PlayerJoinEvent $event)
+    {
+        $p = $event->getPlayer();
+        if (!$p->hasPlayedBefore()) {
+            $this->db->executeInsert("players.insert", [
+                "xuid" => $p->getXuid(),
+                "name" => $p->getName(),
+            ]);
+        }
+        $this->sock->sendData(
+            new \HTNProtocol\Models\PlayerEvent(
+                "join",
+                $p->getName(),
+                null,
+                !$p->hasPlayedBefore()
+            ),
+            "all"
+        );
+    }
+    public function onQuit(PlayerQuitEvent $event)
+    {
+        $p = $event->getPlayer();
+        $this->sock->sendData(
+            new \HTNProtocol\Models\PlayerEvent(
+                "quit",
+                $p->getName(),
+                null,
+                !$p->hasPlayedBefore()
+            ),
+            "all"
+        );
+    }
+    public function onMessage(PlayerChatEvent $event)
+    {
+        $this->sock->sendData(
+            new PlayerMessageEvent(
+                $event->getPlayer()->getName(),
+                $event->getMessage()
+            ),
+            "DiscordBot"
+        );
+    }
+}
