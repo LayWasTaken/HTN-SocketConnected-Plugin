d0cb5582a9b403365befdad1bb043adea1b39a2b
LayWasTaken <clarencetoledo08@gmail.com>
2022-08-30 21:29:08 +0800

    Added temporary response

--- src/Main.php
+++ src/Main.php
@@ -1,64 +1,73 @@
 <?php
 namespace HTNProtocol;
 
-use HTNProtocol\Events\RequestReceivedEvent;
-use HTNProtocol\Models\ServerCrash;
-use HTNProtocol\Models\EconomyEvent;
+use HTNProtocol\Models\Responses\PlayerInfo;
 use pocketmine\command\CommandSender;
 use pocketmine\command\Command;
-use pocketmine\event\block\BlockBreakEvent;
 use pocketmine\event\Listener;
-use pocketmine\event\player\PlayerJoinEvent;
-use pocketmine\event\player\PlayerQuitEvent;
-use pocketmine\item\ItemIds;
 use pocketmine\player\Player;
 use pocketmine\plugin\PluginBase;
 use pocketmine\utils\Config;
-use pocketmine\world\Position;
 use poggit\libasynql\DataConnector;
 use poggit\libasynql\libasynql;
-
-class Main extends PluginBase implements Listener
-{
+class Main extends PluginBase implements Listener {
     public ClientSocket $sock;
     /**
      * @var string[] $LastCrashes
      */
     private array $LastCrashes = [];
     private DataConnector $db;
-    public function onEnable(): void
-    {
-        $this->config = new Config($this->getDataFolder() . "config.yml");
+    public function onEnable(): void {
+        $this->config = new Config($this->getDataFolder() . 'config.yml');
         $this->db = libasynql::create(
             $this,
-            $this->getConfig()->get("database"),
+            $this->getConfig()->get('database'),
             [
-                "mysql" => "mysql.sql",
+                'mysql' => 'mysql.sql',
             ]
         );
-        $this->db->executeGeneric("players.init");
-        $this->db->executeGeneric("discord_codes.init");
+        $this->db->executeGeneric('players.init');
+        $this->db->executeGeneric('discord_codes.init');
         $this->LastCrashes = array_filter(
             scandir(
-                str_replace("plugins\HTNProtocol\src", "crashdumps", __DIR__)
+                str_replace('plugins\HTNProtocol\src', 'crashdumps', __DIR__)
             ),
-            fn($v) => str_contains($v, ".log")
+            fn($v) => str_contains($v, '.log')
         );
         $this->getServer()
             ->getPluginManager()
             ->registerEvents($this, $this);
         $this->sock = new ClientSocket(
-            "localhost",
+            'localhost',
             8080,
-            "09302368772",
-            "Pocketmine",
+            '09302368772',
+            'Pocketmine',
             $this
         );
         $this->sock->registerReceivable(
-            "\HTNProtocol\Streams\JsonStreamClasses\Requests\PlayerPunish",
-            ["DiscordBot"],
-            function () {
-                echo "FOOO";
+            'HTNProtocol\Models\Requests\GetPlayerInfo',
+            ['DiscordBot'],
+            function ($data, $id) {
+                $res = new PlayerInfo();
+                $res->friends = ['Raze', 'Gamerboy'];
+                $res->is_staff = true;
+                $res->kill_stat = ['current_place' => 100, 'kills' => 69];
+                $res->play_time = 1310402840;
+                $res->rank = PlayerInfo::CHALLENGER;
+                $res->money = 1940480;
+                $res->bank_money = 317328478242482;
+                $res->votes = 20;
+                $res->xuid = '699939274294792';
+                $res->status = Playerinfo::ONLINE;
+                $this->sock->sendResponse('DiscordBot', $id, $res);
+            }
+        );
+        $this->sock->registerReceivable(
+            '\HTNProtocol\Models\Requests\PlayerPunish',
+            ['DiscordBot'],
+            function ($data) {
+                var_dump($data);
+                echo 'FOOO';
             }
         );
         $this->getServer()
@@ -72,20 +81,20 @@ class Main extends PluginBase implements Listener
         array $args
     ): bool {
         if ($sender instanceof Player) {
-            if ($command->getName() === "connectdc") {
+            if ($command->getName() === 'connectdc') {
                 if (!array_key_exists(0, $args)) {
                     $sender->sendMessage(
-                        "Must add a discord tag or id to connect"
+                        'Must add a discord tag or id to connect'
                     );
                     return false;
                 }
                 $code = mt_rand(1000, 999999);
                 $this->db->executeInsert(
-                    "discord_codes.insert",
+                    'discord_codes.insert',
                     [
-                        "xuid" => $sender->getXuid(),
-                        "discord" => $args[0],
-                        "code" => $code,
+                        'xuid' => $sender->getXuid(),
+                        'discord' => $args[0],
+                        'code' => $code,
                     ],
                     function () use ($sender, $code) {
                         $sender->sendMessage(
@@ -95,7 +104,7 @@ class Main extends PluginBase implements Listener
                     function ($err) use ($sender) {
                         var_dump($err);
                         $sender->sendMessage(
-                            "Something went wrong try again later"
+                            'Something went wrong try again later'
                         );
                     }
                 );
@@ -103,41 +112,40 @@ class Main extends PluginBase implements Listener
         }
         return true;
     }
-    public function onDisable(): void
-    {
+    public function onDisable(): void {
         $currCrashes = array_values(
             scandir(
-                str_replace("plugins\HTNProtocol\src", "crashdumps", __DIR__),
+                str_replace('plugins\HTNProtocol\src', 'crashdumps', __DIR__),
                 SCANDIR_SORT_DESCENDING
             )
         );
         $crash =
-            $currCrashes[0] === ".last_crash"
+            $currCrashes[0] === '.last_crash'
                 ? $currCrashes[1]
                 : $currCrashes[0];
         if (
             count(
-                array_filter($currCrashes, fn($v) => str_contains($v, ".log"))
+                array_filter($currCrashes, fn($v) => str_contains($v, '.log'))
             ) > count($this->LastCrashes)
         ) {
             $file = @fopen(
-                str_replace("plugins\HTNProtocol\src", "crashdumps", __DIR__) .
+                str_replace('plugins\HTNProtocol\src', 'crashdumps', __DIR__) .
                     "\\$crash",
-                "r",
+                'r',
                 true
             );
             if ($file) {
-                $crash = "";
+                $crash = '';
                 while (($line = fgets($file)) !== false) {
                     if (
-                        strpos($line, "Error: ") === 0 ||
-                        strpos($line, "File: ") === 0 ||
-                        strpos($line, "Line: ") === 0 ||
-                        strpos($line, "Type: ") === 0
+                        strpos($line, 'Error: ') === 0 ||
+                        strpos($line, 'File: ') === 0 ||
+                        strpos($line, 'Line: ') === 0 ||
+                        strpos($line, 'Type: ') === 0
                     ) {
                         $crash .= $line;
                     }
-                    if (strpos($line, "Type: ") === 0) {
+                    if (strpos($line, 'Type: ') === 0) {
                         break;
                     }
                 }
